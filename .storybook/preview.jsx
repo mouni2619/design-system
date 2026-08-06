c65f663acef6af092725f7ed8dffb17c783b2b8e
mouni2619 <mounikaadada26@gmail.com>
2026-08-06 11:32:02 +0530

    Added SCSS variables, typography, and utility styles

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -4,6 +4,9 @@ import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
+import "../src/assets/themes/_variable.scss";
+import "../src/assets/themes/_typography.scss";
+import "../src/assets/themes/_utilities.scss";
 
 /** @type { import('@storybook/react-vite').Preview } */
 const preview = {
