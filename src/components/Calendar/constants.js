10758e9de3f2cc65630f65f24ad7c851c2cce642
mouni2619 <mounikaadada26@gmail.com>
2026-09-23 14:25:47 +0530

    Removed commented code

--- src/components/Calendar/constants.js
+++ src/components/Calendar/constants.js
@@ -1,5 +1,3 @@
-/* -- Calendar ----------------------------------------------------------------- */
-// "single": one month, one date. "range": two months, a start/end date pair.
 export const CALENDAR_MODES = ["single", "range"];
 
 export const DEFAULT_CALENDAR_MODE = "single";
